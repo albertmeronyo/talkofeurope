3b55956fc082977c25d67dc47ee3e33d464d6f59
Albert Meroño-Peñuela <albert.meronyo@gmail.com>
2014-10-08 10:25:42 +0200

    cached csv yearly title counts

--- sparql.py
+++ sparql.py
@@ -2,6 +2,38 @@
 
 from SPARQLWrapper import SPARQLWrapper, JSON
 import csv
+import cStringIO
+import codecs
+
+class UnicodeWriter:
+    """
+    A CSV writer which will write rows to CSV file "f",
+    which is encoded in the given encoding.
+    """
+
+    def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
+        # Redirect output to a queue
+        self.queue = cStringIO.StringIO()
+        self.writer = csv.writer(self.queue, dialect=dialect, **kwds)
+        self.stream = f
+        self.encoder = codecs.getincrementalencoder(encoding)()
+
+    def writerow(self, row):
+        self.writer.writerow([s.encode("utf-8") for s in row])
+        # Fetch UTF-8 output from the queue ...
+        data = self.queue.getvalue()
+        data = data.decode("utf-8")
+        # ... and reencode it into the target encoding
+        data = self.encoder.encode(data)
+        # write to the target stream
+        self.stream.write(data)
+        # empty queue
+        self.queue.truncate(0)
+
+    def writerows(self, rows):
+        for row in rows:
+            self.writerow(row)
+
  
 sparql = SPARQLWrapper("http://linkedpolitics.ops.few.vu.nl/sparql/")
 
@@ -37,9 +69,11 @@ for year in time_range:
     results = sparql.query().convert()
 
     with open('out-%s.csv' % str(year), 'wb') as csvfile:
-        writer = csv.writer(csvfile, delimiter='\t',
+        writer = UnicodeWriter(csvfile, delimiter='\t',
                             quotechar='"', quoting=csv.QUOTE_MINIMAL)
         writer.writerow(['title', 'count'])
         for result in results["results"]["bindings"]:
-            writer.writerow([unicode(result["title"]["value"], 'utf-8'),
-                             unicode(result["count"]["value"], 'utf-8')])
+            writer.writerow([result["title"]["value"],
+                             result["count"]["value"]])
+
+
